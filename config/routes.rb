fdd3207f66a73c8fe45d7ab84cea8a306a7bed46
huntertparks <hunter.thomas.parks@gmail.com>
2018-05-20 15:21:24 -0700

    add posts into routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,8 @@ Rails.application.routes.draw do
   scope module: 'users' do
     resources :profiles
   end
+  namespace :users do
+    resources :posts
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
