f28d67dec9c2de7afd343247ba5894c3c46765db
huntertparks <hunter.thomas.parks@gmail.com>
2018-05-20 13:09:21 -0700

    add routes for user profiles

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   devise_for :users, path: 'users', controllers: {
     sessions: "users/sessions"
   }
+
+  get 'profile', action: :index, controller: 'users'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
