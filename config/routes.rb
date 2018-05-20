44b92ade30eab56a83bf68c6a1388318ec51a32d
huntertparks <hunter.thomas.parks@gmail.com>
2018-05-20 14:10:38 -0700

    add profiles routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
     sessions: "users/sessions"
   }
 
-  get 'profile', action: :index, controller: 'users'
+  resources :profiles, module: 'users'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
