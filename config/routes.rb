da5cfe604ea8e9cceef911fc22bd36b6ca8f5961
huntertparks <hunter.thomas.parks@gmail.com>
2018-05-20 16:29:49 -0700

    edit order of routes for organization purposes

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,13 @@ Rails.application.routes.draw do
   devise_for :users, path: 'users', controllers: {
     sessions: "users/sessions"
   }
-
+  namespace :users do
+      resources :posts
+    end
+    
   scope module: 'users' do
     resources :profiles
   end
-  namespace :users do
-    resources :posts
-  end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
