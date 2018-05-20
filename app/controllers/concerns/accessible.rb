1b9f5c1a13c69fa03aff9822b576c9904761eeec
huntertparks <hunter.thomas.parks@gmail.com>
2018-05-19 20:33:37 -0700

    format protected method to squish bugs

--- app/controllers/concerns/accessible.rb
+++ app/controllers/concerns/accessible.rb
@@ -5,14 +5,13 @@ module Accessible
     end
 
     protected
-        def :check_user
-            if current_admin
-                flash.clear
-                redirect_to(rails_admin.dashboard_path) && return
-            elsif current_user
-                flash.clear
-                redirect_to(authenticated_user_root_path) && return
-            end
+    def check_user
+        if current_admin
+            flash.clear
+            redirect_to(rails_admin.dashboard_path) && return
+        elsif current_user
+            flash.clear
+            redirect_to(authenticated_user_root_path) && return
         end
     end
 end
