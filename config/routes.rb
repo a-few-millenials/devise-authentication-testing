0e95711ea9132090da09e836c53f2e96cfa59dfc
huntertparks <hunter.thomas.parks@gmail.com>
2018-05-19 17:15:19 -0700

    link admins to admin sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :admins, path: 'admins'
+  devise_for :admins, path: 'admins', controllers: {
+    sessions: "admins/sessions"
+  }
   devise_for :users, path: 'users'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
