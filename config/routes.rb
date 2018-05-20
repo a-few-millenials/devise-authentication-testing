48e9d96945742564b3c213793deb95d007dda1cb
huntertparks <hunter.thomas.parks@gmail.com>
2018-05-19 17:12:32 -0700

    add names to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+  devise_for :admins, path: 'admins'
+  devise_for :users, path: 'users'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
