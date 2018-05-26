247136b28cf7e4b3f08f6b751482934f939fce36
huntertparks <hunter.thomas.parks@gmail.com>
2018-05-26 14:52:21 -0700

    remove comments

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,4 @@ Rails.application.routes.draw do
   scope module: 'users' do
     resources :profiles
   end
-  
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
