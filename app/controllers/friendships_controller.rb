8189569907e36dbe2691e71112fc207ab54511aa
huntertparks <hunter.thomas.parks@gmail.com>
2018-05-26 23:14:54 -0700

    add create action that will create friendships and redirect to same page

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,3 +1,12 @@
 class FriendshipsController < ApplicationController
-
+    def create
+        @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
+        if @friendship.save
+            flash[:notice] = "Added Friend"
+            redirect_to action: "admins/users#index"
+        else
+            flash[:notice] = "Unable to add friend"
+            redirect_to action: "admins/users#index"
+        end
+    end
 end
