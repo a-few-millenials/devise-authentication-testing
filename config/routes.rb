70fb6c86b74a9880c2758156573de2134c5b23ea
huntertparks <hunter.thomas.parks@gmail.com>
2018-05-27 00:12:35 -0700

    delete friendship association and edit routes to be more dry

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   scope module: 'users' do
-    resources :feed
+    resources :feed, :profiles
   end
 
   devise_for :admins, path: 'admins', controllers: {
@@ -17,9 +17,5 @@ Rails.application.routes.draw do
   end
   namespace :admins do
     resources :users
-  end
-    
-  scope module: 'users' do
-    resources :profiles, :friendships
   end
 end
