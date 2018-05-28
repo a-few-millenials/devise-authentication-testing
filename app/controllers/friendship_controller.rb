1b1188f840d284de4bd110b57f752d1361e8b1a7
huntertparks <hunter.thomas.parks@gmail.com>
2018-05-27 23:03:41 -0700

    add entries into friendship database into send_request action | initiate relations between users

--- app/controllers/friendship_controller.rb
+++ app/controllers/friendship_controller.rb
@@ -1,6 +1,9 @@
 class FriendshipController < ApplicationController
     def send_request
-
+        friendship = Friendship.new do |f|
+            f.send_request = @user
+            f.accept_request = @friend
+        end
     end
 
     def accept_request
