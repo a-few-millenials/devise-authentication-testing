a6dd8f261150718a49f40e2bc3de56f354de65a5
huntertparks <hunter.thomas.parks@gmail.com>
2018-05-27 23:29:04 -0700

    add find users methods to both actions

--- app/controllers/friendship_controller.rb
+++ app/controllers/friendship_controller.rb
@@ -1,5 +1,7 @@
 class FriendshipController < ApplicationController
     def send_request
+        @user = User.find(params[:user])
+        @friend = User.find(params[:friend])
         friendship = Friendship.new do |f|
             f.send_request = @user
             f.accept_request = @friend
@@ -9,6 +11,8 @@ class FriendshipController < ApplicationController
     end
 
     def accept_request
+        @user = User.find(params[:user])
+        @friend = User.find(params[:friend])
         friendship = Friendship.where(accept_request: params[:user], send_request: params[:friend])
         friendship.accepted = true
         friendship.save
