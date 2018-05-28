8b5384d57c35dbd08b274dc9539aa7d37b7d591a
huntertparks <hunter.thomas.parks@gmail.com>
2018-05-28 11:39:36 -0700

    add debugging code and use the ID's of users and friends to save into database instead of entire model to squish bugs

--- app/controllers/friendship_controller.rb
+++ app/controllers/friendship_controller.rb
@@ -2,9 +2,10 @@ class FriendshipController < ApplicationController
     def send_request
         @user = User.find(params[:user])
         @friend = User.find(params[:friend])
-        friendship = Friendship.new(request_user_id: @user, accept_user_id: @friend)
+        friendship = Friendship.new(request_user_id: @user.id, accept_user_id: @friend.id)
         logger.debug "Current User: #{@user.attributes.inspect}"
         logger.debug "Other User : #{@friend.attributes.inspect}"
+        logger.debug "Friendship: #{friendship.attributes.inspect}"
         friendship.save
         redirect_to "/"
     end
