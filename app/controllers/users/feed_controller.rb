83fc4ab3cdc9aa9aa54dd5294debe9785b8496a0
huntertparks <hunter.thomas.parks@gmail.com>
2018-05-28 13:17:14 -0700

    add for loop to get list of users and their relationships with debugging tools

--- app/controllers/users/feed_controller.rb
+++ app/controllers/users/feed_controller.rb
@@ -6,6 +6,14 @@ class Users::FeedController < ApplicationController
   end
 
   def friendRequests
-
+    users = User.join("INNER JOIN friendships ON users.id = friendships.request_user_id")
+    users_array =  Array.new
+    users.each do |u|
+      if u.request_user_id = NULL OR u.accepted = NULL
+      else
+        users_array.concat(u)
+      end
+    end
+    logger.debug "Current array: #{users_array.attributes.inspect}"
   end
 end
