584fe4f9fc0d491a8bfe71c9f26faa5fe3113841
huntertparks <hunter.thomas.parks@gmail.com>
2018-05-28 13:59:51 -0700

    edit view and controller to correctly go through correct action to get to view page

--- app/controllers/users/feed_controller.rb
+++ app/controllers/users/feed_controller.rb
@@ -1,19 +1,20 @@
 class Users::FeedController < ApplicationController
   def index
+    logger.debug "Current array: #{users.attributes.inspect}"
   end
 
   def new
+    logger.debug "Current array: #{users.attributes.inspect}"
   end
 
-  def friendRequests
-    users = User.join("INNER JOIN friendships ON users.id = friendships.request_user_id")
-    @array = Array.new
-    users.each do |u|
-      if u.request_user_id is NULL OR u.accepted is NULL
-      else
-        @array.concat(u)
-      end
-    end
-    logger.debug "Current array: #{users_array.attributes.inspect}"
+  def friend_request
+    @users = User.joins("INNER JOIN friendships ON users.id = friendships.request_user_id")
+    #@array = Array.new
+    #users.each do |u|
+      #if u.request_user_id is NULL OR u.accepted is NULL
+      #else
+        #@array.push(u)
+      #end
+    #end
   end
 end
