355b80a74e528ac30371a0009c63d6ed934845be
huntertparks <hunter.thomas.parks@gmail.com>
2018-05-28 20:40:39 -0700

    add if statements to not display own user and add debug lines in controller

--- app/controllers/users/feed_controller.rb
+++ app/controllers/users/feed_controller.rb
@@ -9,7 +9,9 @@ class Users::FeedController < ApplicationController
 
   def friend_request
     @Users = User.get_friends
+    @app_current_user = current_user
     
+    logger.debug "App Current User: #{@app_current_user.attributes.inspect}"
     #@users = User.joins("INNER JOIN friendships ON users.id = friendships.request_user_id INNER JOIN users ON friendships.accept_user_id = users.id")
     #dropTable = "DROP TEMPORARY TABLE IF NOT EXISTS temp_users;"
     #createTable = "CREATE TEMPORARY TABLE IF NOT EXISTS temp_users (temp_id BIGINT(20) NOT NULL,temp_email VARCHAR(255) NOT NULL,temp_encrypted_password VARCHAR(255) NOT NULL,temp_reset_password_token varchar(255),temp_reset_password_sent_at DATETIME,temp_remember_created_at DATETIME,temp_sign_in_count INT(11) NOT NULL,temp_current_sign_in_at DATETIME,temp_last_sign_in_at DATETIME,temp_current_sign_in_ip VARCHAR(255),temp_last_sign_in_ip VARCHAR(255),temp_created_at DATETIME NOT NULL,temp_updated_at DATETIME);"
