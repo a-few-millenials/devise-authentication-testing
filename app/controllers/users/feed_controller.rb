14d0b3498a9caefca63c214780caa90be3e31e82
huntertparks <hunter.thomas.parks@gmail.com>
2018-05-28 19:44:58 -0700

    fix syntax | User model now has a scope to (with correct convention) get list of all friends and display it

--- app/controllers/users/feed_controller.rb
+++ app/controllers/users/feed_controller.rb
@@ -8,13 +8,17 @@ class Users::FeedController < ApplicationController
   end
 
   def friend_request
-    @users = User.joins("INNER JOIN friendships ON users.id = friendships.request_user_id")
-    #@array = Array.new
-    #users.each do |u|
-      #if u.request_user_id is NULL OR u.accepted is NULL
-      #else
-        #@array.push(u)
-      #end
-    #end
+    @Users = User.get_friends
+    
+    #@users = User.joins("INNER JOIN friendships ON users.id = friendships.request_user_id INNER JOIN users ON friendships.accept_user_id = users.id")
+    #dropTable = "DROP TEMPORARY TABLE IF NOT EXISTS temp_users;"
+    #createTable = "CREATE TEMPORARY TABLE IF NOT EXISTS temp_users (temp_id BIGINT(20) NOT NULL,temp_email VARCHAR(255) NOT NULL,temp_encrypted_password VARCHAR(255) NOT NULL,temp_reset_password_token varchar(255),temp_reset_password_sent_at DATETIME,temp_remember_created_at DATETIME,temp_sign_in_count INT(11) NOT NULL,temp_current_sign_in_at DATETIME,temp_last_sign_in_at DATETIME,temp_current_sign_in_ip VARCHAR(255),temp_last_sign_in_ip VARCHAR(255),temp_created_at DATETIME NOT NULL,temp_updated_at DATETIME);"
+    #insertTable = "INSERT INTO temp_users SELECT * FROM users;"
+    #joinTable = "SELECT * FROM users INNER JOIN friendships ON users.id = friendships.request_user_id INNER JOIN temp_users ON friendships.accept_user_id = temp_users.temp_id;"
+
+    #ActiveRecord::Base.connection.execute(dropTable)
+    #ActiveRecord::Base.connection.execute(createTable)
+    #ActiveRecord::Base.connection.execute(insertTable)
+    #@users = ActiveRecord::Base.connection.execute(joinTable)
   end
 end
