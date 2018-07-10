98430d117184211f1bf61587c37abd9ae3496db6
huntertparks <hunter.thomas.parks@gmail.com>
2018-07-09 21:46:46 -0700

    add model testing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,9 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "Create new user" do
+    user = User.new
+    assert_not user.save
+  end
 end
