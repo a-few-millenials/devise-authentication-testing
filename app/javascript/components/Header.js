2dbc4a7a3766ca20e7077f2e01c881ba1fb6fa2a
huntertparks <hunter.thomas.parks@gmail.com>
2018-07-09 21:17:01 -0700

    add react component labeled 'header'

---
+++ app/javascript/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Header extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        Title: {this.props.title}
+      </React.Fragment>
+    );
+  }
+}
+
+Header.propTypes = {
+  title: PropTypes.string
+};
+export default Header
