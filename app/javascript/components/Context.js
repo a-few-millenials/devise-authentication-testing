b7415b5f1b95588c8046e8d137641fbbf4e2af01
huntertparks <hunter.thomas.parks@gmail.com>
2018-07-14 14:19:49 -0700

    add states to the context button to display log out/log in buttons and account settings

--- app/javascript/components/Context.js
+++ app/javascript/components/Context.js
@@ -1,11 +1,28 @@
 import React from "react"
 import PropTypes from "prop-types"
+import DropdownMenu from "./DropdownMenu"
 class Context extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      active: false,
+    };
+  }
+
+  handleClick() {
+    const currentState = this.state.active;
+    this.setState({
+      active: !currentState
+    });
+  }
+
   render () {
     return (
       <React.Fragment>
         {this.props.children}
-        Menu
+        <span className="context-menu" onClick={this.handleClick}>Menu</span>
+        <span className={this.state.active ? 'dropdown-list': 'dropdown-list-full'}><DropdownMenu /></span>
       </React.Fragment>
     );
   }
--- app/javascript/components/DropdownMenu.js
+++ app/javascript/components/DropdownMenu.js
@@ -4,6 +4,9 @@ class DropdownMenu extends React.Component {
   render () {
     return (
       <React.Fragment>
+        {this.props.children}
+        <a href="#">Account Settings</a>
+        <a href="#">Log Out</a>
       </React.Fragment>
     );
   }
