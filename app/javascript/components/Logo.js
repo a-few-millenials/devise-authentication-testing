546cfcd4eeab27f7fb90f7d1e2c6d76eafb234be
huntertparks <hunter.thomas.parks@gmail.com>
2018-07-14 19:12:27 -0700

    add correct logo importing to component

--- app/javascript/components/Logo.js
+++ app/javascript/components/Logo.js
@@ -1,10 +1,11 @@
 import React from "react"
 import PropTypes from "prop-types"
+import Logo from 'C:/Users/adria/Pictures/Logo.gif'
 class Logo extends React.Component {
   render () {
     return (
       <React.Fragment>
-        <img src="C:\Users\adria\Pictures\Logo.gif">
+        <img src={Logo} />
       </React.Fragment>
     );
   }
