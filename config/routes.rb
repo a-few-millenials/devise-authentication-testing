c45f2bb7367bb4bef8b8e74b9f5199fcec353c62
huntertparks <hunter.thomas.parks@gmail.com>
2018-07-15 22:54:29 -0700

    add get route for deleting user session

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions'
-  }
-
+  } do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   resources :home
   
   root to: "home#index"
