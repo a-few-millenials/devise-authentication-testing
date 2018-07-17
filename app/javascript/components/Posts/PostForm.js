529f4762ed21ffba0a0255471b85d4a2e8bf4a07
huntertparks <hunter.thomas.parks@gmail.com>
2018-07-16 19:07:09 -0700

    move Postform.js into its own direcgtory and add a form with functions that handle submit and change

---
+++ app/javascript/components/Posts/PostForm.js
@@ -0,0 +1,35 @@
+import React from "react"
+import PropTypes from "prop-types"
+class PostForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A post was submitted: ' + this.state);
+    event.preventDefault();
+  }
+
+  render () {
+    return (
+      <React.Fragment>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Image:
+            <input type="image" value={this.state.value} onChange={this.handleChange} />
+          </label>
+        </form>
+      </React.Fragment>
+    );
+  }
+}
+
+export default PostForm
