bcbc98ecc8ec33ac3a7abcb84b439dc27efb99c4
huntertparks <hunter.thomas.parks@gmail.com>
2018-07-16 19:31:13 -0700

    update rails to 5.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,3 @@
 Rails.application.routes.draw do
-  get 'wall/index'
-  get 'wall/new'
-
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  } do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
-  resources :home
-
-  root to: "home#index"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
