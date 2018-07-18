f5eed90ae5384aacbadadd705323e97f47487554
huntertparks <hunter.thomas.parks@gmail.com>
2018-07-17 19:32:38 -0700

    add private variable 'post_params', add redirect in create action, and add call for all Posts in index

--- app/controllers/wall_controller.rb
+++ app/controllers/wall_controller.rb
@@ -1,5 +1,6 @@
 class WallController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
@@ -8,5 +9,10 @@ class WallController < ApplicationController
 
   def create
     @post = Post.create(post_params)
+    redirect_to wall_index_url
+  end
+
+  def post_params
+    params.require(:post).permit(:image, :caption)
   end
 end
