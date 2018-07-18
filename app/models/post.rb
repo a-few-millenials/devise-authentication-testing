215d43e67e6ee94c01e794d0d346e355ac460862
huntertparks <hunter.thomas.parks@gmail.com>
2018-07-17 20:13:31 -0700

    remove validation for captions and add styling for images

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-    validates :image, :caption presence: true
+    validates :image, presence: true
 
-    has_attached_file :image, styles: { :medium => "640x" }
+    has_attached_file :image, styles: { :medium => "300x300>", thumb: "100x100>" }
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
