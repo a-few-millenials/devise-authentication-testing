66c0675f6dd16e8e4e56d8af2678a2fa7a3f927a
huntertparks <hunter.thomas.parks@gmail.com>
2018-07-17 20:33:10 -0700

    migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180717011550) do
+ActiveRecord::Schema.define(version: 20180718033057) do
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "caption"
@@ -35,6 +35,10 @@ ActiveRecord::Schema.define(version: 20180717011550) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
