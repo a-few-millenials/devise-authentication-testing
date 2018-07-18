f2ce54ca18bbe6d9585ed8d458b7007f24d06e3c
huntertparks <hunter.thomas.parks@gmail.com>
2018-07-17 20:31:21 -0700

    add images to users to act as profile picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_attached_file :image, styles: { :thumb => "100x100" }
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
