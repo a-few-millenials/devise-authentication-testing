ccb23cba648644040c3958ec338ffe74c7a2cd08
huntertparks <hunter.thomas.parks@gmail.com>
2018-07-18 17:53:59 -0700

    add has_and_belongs to users to start friendship project

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_and_belongs_to_many(:users,
+    :join_table => "friendships",
+    :foreign_key => "request_user_id",
+    :association_foreign_key => "accept_user_id")
+
   has_attached_file :image, styles: { :thumb => "100x100" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
