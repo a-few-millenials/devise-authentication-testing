cad13f0f9d49fcb3856ef4111cdaae97a8a2feb7
huntertparks <hunter.thomas.parks@gmail.com>
2018-07-18 18:46:04 -0700

    edit syntax error to squish bugs

--- app/controllers/friendship_controller.rb
+++ app/controllers/friendship_controller.rb
@@ -6,7 +6,7 @@ class FriendshipController < ApplicationController
   def send_request
     @user = current_user
     @friend = User.find(params[:friend])
-    friendship = Friendship.new(request_user_id: @user.id, accept_userid: @friend.id)
+    friendship = Friendship.new(request_user_id: @user.id, accept_user_id: @friend.id)
     friendship.save
     redirect_to "/"
   end
