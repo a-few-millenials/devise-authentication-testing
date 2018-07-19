2ca295ffd8cec1576b6bf4d2b8ffa82b72da53f2
huntertparks <hunter.thomas.parks@gmail.com>
2018-07-18 18:08:05 -0700

    add context to action 'send_request' to send a friend request to another account

--- app/controllers/friendship_controller.rb
+++ app/controllers/friendship_controller.rb
@@ -1,5 +1,10 @@
 class FriendshipController < ApplicationController
   def send_request
+    @user = current_user
+    @friend = User.find(params[:friend])
+    friendship = Friendship.new(request_user_id: @user.id, accept_userid: @friend.id)
+    friendship.save
+    redirect_to "/"
   end
 
   def accept_request
