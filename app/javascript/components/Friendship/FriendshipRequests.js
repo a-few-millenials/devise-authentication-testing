8a6c7df4279be78feeea927fe4cf192cac0f028d
huntertparks <hunter.thomas.parks@gmail.com>
2018-07-18 23:14:32 -0700

    add code to display list of users as a component

--- app/javascript/components/Friendship/FriendshipRequests.js
+++ app/javascript/components/Friendship/FriendshipRequests.js
@@ -1,12 +1,32 @@
 import React from "react"
 import PropTypes from "prop-types"
 class FriendshipRequests extends React.Component {
-  render () {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: props.list,
+    };
+  }
+
+  render (props) {
     return (
       <React.Fragment>
+        <ul>
+          {
+            this.state.data.map((user) => 
+              <li key={user.email}>
+                {user.email}
+              </li>
+            )
+          } 
+        </ul>
       </React.Fragment>
     );
   }
+}
+
+FriendshipRequests.proptypes = {
+  list: PropTypes.array
 }
 
 export default FriendshipRequests
