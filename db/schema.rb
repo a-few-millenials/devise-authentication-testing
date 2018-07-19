3973610541ca9c8f413d231e26cb249907e4a548
huntertparks <hunter.thomas.parks@gmail.com>
2018-07-18 17:57:03 -0700

    migrate db for friendships

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180718033057) do
+ActiveRecord::Schema.define(version: 20180719005559) do
+
+  create_table "friendships", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "request_user_id"
+    t.integer "accept_user_id"
+    t.boolean "accepted"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "caption"
