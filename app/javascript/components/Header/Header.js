34f4c4b963166cfc6f23ab1a8fde26101e238956
huntertparks <hunter.thomas.parks@gmail.com>
2018-07-21 23:24:49 -0700

    add FriendRequest react component to header

--- app/javascript/components/Header/Header.js
+++ app/javascript/components/Header/Header.js
@@ -2,6 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import Context from './Context'
 import Logo from './Logo'
+import FriendRequests from './FriendRequests'
 class Header extends React.Component {
   constructor(props) {
     super(props);
@@ -16,6 +17,7 @@ class Header extends React.Component {
             Please make the context menu change dynamically based on user status (logged in or logged out)
             https://stackoverflow.com/questions/36205673/how-do-i-create-a-dynamic-drop-down-list-with-react-bootstrap
           */}
+          <FriendRequests />
           <Context />
         </div>
       </React.Fragment>
