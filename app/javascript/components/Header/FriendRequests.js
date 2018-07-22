a25fc6c44f45b87adfbf98344de792b6e378bc14
huntertparks <hunter.thomas.parks@gmail.com>
2018-07-22 00:55:12 -0700

    add constructor for FriendRequest react component

--- app/javascript/components/Header/FriendRequests.js
+++ app/javascript/components/Header/FriendRequests.js
@@ -1,6 +1,10 @@
 import React from "react"
 import PropTypes from "prop-types"
 class FriendRequests extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render () {
     return (
       <React.Fragment>
