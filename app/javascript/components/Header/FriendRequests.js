112d6ee77de49f05fd228528e9884d81eb20eddc
huntertparks <hunter.thomas.parks@gmail.com>
2018-07-22 00:59:36 -0700

    add react-rails via import to 'FriendRequest'

--- app/javascript/components/Header/FriendRequests.js
+++ app/javascript/components/Header/FriendRequests.js
@@ -1,5 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
+import ReactList from "react-list"
 class FriendRequests extends React.Component {
   constructor(props) {
     super(props);
