3d40599ad3d770f441bb564b07457e6a670785ec
huntertparks <hunter.thomas.parks@gmail.com>
2018-07-22 19:27:57 -0700

    add empty constructor and componentDidMount methods

--- app/javascript/components/Header/FriendRequestList.js
+++ app/javascript/components/Header/FriendRequestList.js
@@ -1,9 +1,23 @@
 import React from "react"
 import PropTypes from "prop-types"
 class FriendRequestList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    
+  }
+
   render () {
     return (
       <React.Fragment>
+        {/*
+          Use Ajax to get list of Friend requests from current user
+
+          Put Ajax call into componentWillMount() method and pull array of users that have sent 
+          current user a friend request
+        */}
       </React.Fragment>
     );
   }
