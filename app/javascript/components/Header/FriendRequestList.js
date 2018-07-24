3d17e797f2372aa4b58bbebc1aee3e4e9fbd6d56
huntertparks <hunter.thomas.parks@gmail.com>
2018-07-23 17:24:46 -0700

    add states, ajax requests, and hypertext that renders when FriendRequest component is called

--- app/javascript/components/Header/FriendRequestList.js
+++ app/javascript/components/Header/FriendRequestList.js
@@ -3,13 +3,47 @@ import PropTypes from "prop-types"
 class FriendRequestList extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
   }
 
   componentDidMount() {
-    
+    fetch("").then(res => res.json()).then(
+      (result) => {
+       this.setState({
+         isLoaded: true,
+         items: result.items
+       }); 
+      },
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    )
   }
 
   render () {
+    const { error, isLoaded, items } = this.state;
+    if(error) {
+      return <div>Error: {error.message}</div>
+    } else if (!isLoaded) {
+      return <div>Loading...</div>
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.id}>
+              {item.email}
+            </li>
+          ))}
+        </ul>
+      )
+    }
     return (
       <React.Fragment>
         {/*
