575d640e2f916315fce0d11e0d8af7babae74d5e
huntertparks <hunter.thomas.parks@gmail.com>
2018-07-24 21:21:06 -0700

    add functions for FriendRequests react component

--- app/javascript/components/Header/FriendRequests.js
+++ app/javascript/components/Header/FriendRequests.js
@@ -3,26 +3,45 @@ import PropTypes from "prop-types"
 class FriendRequests extends React.Component {
   constructor(props) {
     super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+    this.setWrapperRef= this.setWrapperRef.bind(this);
     this.state = {
       active: false
     }
   }
 
-  handleClick() {
+  componentDidMount() {
+    document.addEventListener('mousedown', this.handleOutsideClick, false);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleOutsideClick, false);
+  }
+
+  handleClick(e) {
+    if (this.node.contains(e.target)) {
+      return;
+    }
+
     const currentState = this.state.active;
     this.setState({
       active: !currentState
     });
   }
 
+  setWrapperRef(node) {
+    this.wrapperRef = node;
+  }
+
   render () {
     return (
       <React.Fragment>
-        <div onClick={this.handleClick}>
+        <div onClick={this.handleClick} ref={this.setWrapperRef}>
           {this.props.children}
           <span className="friend-requests">F</span>
           <div className={this.state.active ? 'friend-requests-full': 'friend-requests-none'}>
-            
+            <h2>Testing</h2>
           </div>
         </div>
       </React.Fragment>
