8f333bf9dfc824292308e6f6c61bf3e7ddd32423
huntertparks <hunter.thomas.parks@gmail.com>
2018-07-25 22:31:57 -0700

    add methods and event handlers for displaying list of Friendship requests

--- app/javascript/components/Header/FriendRequests.js
+++ app/javascript/components/Header/FriendRequests.js
@@ -4,7 +4,11 @@ import { Dropdown, DropdownButton, SplitButton } from "react-bootstrap"
 class FriendRequests extends React.Component {
   constructor(props) {
     super(props);
-      this.state = {
+
+    this.setWrapperRef = this.setWrapperRef.bind(this);
+    this.setChildRef = this.setChildRef.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
       active: false
     }
   }
@@ -17,14 +21,35 @@ class FriendRequests extends React.Component {
     document.removeEventListener('mousedown', this.handleClick, false);
   }
 
+  handleClick(e) {
+    if(!this.wrapperRef.contains(e.target) && !this.state.active) {
+      return;
+    } else if (this.childRef.contains(e.target)) {
+      return;
+    }
+
+    const currentState = this.state.active;
+    this.setState({
+      active: !currentState
+    });
+  }
+
+  setWrapperRef(node) {
+    this.wrapperRef = node;
+  }
+
+  setChildRef(node) {
+    this.childRef = node;
+  }
+
   render () {
     return (
       <React.Fragment>
         <div onClick={this.handleClick} ref={this.setWrapperRef}>
           {this.props.children}
-          <span className="friend-requests">F</span>
-          <div className={this.state.active ? 'friend-requests-full': 'friend-requests-none'}>
-
+          <span className="friend-requests" ref={this.setWrapperRef}>F</span>
+          <div className={this.state.active ? 'friend-requests-full': 'friend-requests-none'} ref={this.setChildRef}>
+            <FriendRequestList />
           </div>
         </div>
       </React.Fragment>
